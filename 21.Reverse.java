c78e9b6add4f262432f1e7c5644ccc855807ff49
Jayaram192210314 <sn3029769@gmail.com>
2024-06-03 17:26:06 +0530

    jk

---
+++ 21.Reverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class king
+{
+	public static void main(String a[])
+{
+	Scanner s = new Scanner(System.in);
+	int rev=0,i,n;
+	System.out.println("Enter the Number:");
+	n = s.nextInt();
+	while(n!=0)
+	{
+		i = n%10;
+		rev = rev*10 + i;
+		n = n/10;
+	}
+	System.out.println("The Number After Reverse:"+rev);
+}
+}
